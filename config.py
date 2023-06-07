13ad3b5a64a7a5b53e70b8510a7485511d853048
Demos Petsas <demos@petsas.consulting>
2023-06-07 12:53:54 +0300

    update token expiration

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
 JWT_TOKEN_LOCATION = ["headers"]
 JWT_IDENTITY_CLAIM = "user_id"  # default == sub
-JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=5)
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
